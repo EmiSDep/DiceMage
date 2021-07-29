73f2696644c3f9008766751b5c664a8fa37369a1
Emilio Depena <emiliosdep@gmail.com>
2021-07-29 18:58:10 -0400

    Initial commit

---
+++ src/com/emisdep/Player.java
@@ -0,0 +1,16 @@
+package com.emisdep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    public String name = "";
+    int health = 0;
+    int mana = 0;
+    int power = 6;
+    List<Die> monsterDen = new ArrayList<>();
+
+    
+
+}
